63c95b46397905279daef6be1c2d63875b553afd
srayo16 <srayosikder@gmail.com>
2023-04-25 11:32:00 +0600

    counting

--- countingArray.c
+++ countingArray.c
@@ -2,7 +2,22 @@
 #include <string.h>
 
 int main()
-{
+{   
+    char a[100];
+    scanf("%s", a);
+    int count[26] ={0};
+    for (int i = 0; i < strlen(a); i++)
+    {
+        char value = a[i]-'a';
+        // printf("%d ", value);
+        count[value]++;    
+    }
 
+    for (int i = 0; i < 26; i++)
+    {
+        printf("%c = %d\n", i+97, count[i]);
+    }
+    
+    
     return 0;
 }
